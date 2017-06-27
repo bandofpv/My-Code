645a6614c8c4c1ebbd1d8d62adf5a5939ed4ff89
bandofpv <andobernas@gmail.com>
2017-06-27 13:35:46 -0700

    Update Fan_Test.ino

--- Fan_Test.ino
+++ Fan_Test.ino
@@ -1,12 +1,12 @@
-const int fanmotor = 12;
+const int fanmotor = 12; //number of fan motor pin
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(fanmotor, OUTPUT);
+  pinMode(fanmotor, OUTPUT); //setting the fan motor pin to an OUTPUT
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(fanmotor, HIGH);
+  digitalWrite(fanmotor, HIGH); //turn on the fan motor. 
  
 }
