84a6f863b8944ce2278150e3280b55199110c17d
bandofpv <andobernas@gmail.com>
2017-06-28 07:36:53 -0700

    Update motor_direction_test.ino

--- motor_direction_test.ino
+++ motor_direction_test.ino
@@ -1,9 +1,12 @@
+/*
+This code is used to test the direction of the motors.
+ */
 const int dcmotor1pin1 = 3; //sets in1 to pin 3
 const int dcmotor1pin2 = 5; //sets in2 to pin 5
 const int dcmotor2pin1 = 6; //sets in3 to pin 6
 const int dcmotor2pin2 = 9; //sets in4to pin 9
-const int pwmMax = 160;
-const int pwmMin = 70;
+const int pwmMax = 160; //sets pwmMax to 160
+const int pwmMin = 70; //sets pwmMin to 70
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,8 +18,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-   digitalWrite(dcmotor1pin1, pwmMin+20); //sets in1 to 70pwm
+   digitalWrite(dcmotor1pin1, pwmMin+20); //sets in1 to 90pwm
    digitalWrite(dcmotor1pin2, 0); //sets in2 to 0
    digitalWrite(dcmotor2pin1, 0); //sets in3 to 0
-   digitalWrite(dcmotor2pin2, pwmMin+20); //sets in4 to 70pwm
+   digitalWrite(dcmotor2pin2, pwmMin+20); //sets in4 to 90pwm
 }
